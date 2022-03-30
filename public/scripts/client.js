8be8a6b36205592a8279ec73fd73eeaafbb96876
Culganosi <culgan_@hotmail.com>
2022-03-30 14:24:03 -0400

    pre-ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -30,16 +30,17 @@ const data = [
   }
 ]
 
-const createTweetElement = function(tweetGen) {
-  let $tweet = <article class="tweet">
+const createTweetElement = function(tweetObject) {
+  let $tweet = (`
+  <article class="tweet">
     <header>
-      <img src ="https://icon-library.com/images/avatar-icon-images/avatar-icon-images-7.jpg" alt="avatar goes here" class="avatars"></img>
-      <h2 class="handle">@PENGUINS4LYFE</h2>
-      <p class="name">Oswald Cobblepot</p>
+      <img src ="${tweetObject.user.avatars}" alt="avatar goes here" class="avatars">
+      <h2 class="handle">${tweetObject.user.handle}</h2>
+      <p class="name">${tweetObject.user.name}</p>
     </header>
     <div class="tweets-main">
-    <p class="content"> hey im new here anyone else like penguins or what </p>
-    <span class="daysAgo">5 days ago</span>
+    <p class="content">${tweetObject.content.text}</p>
+    <span class="daysAgo">${tweetObject.content.created_at}</span>
     <span class="icons">
     <i class="fas fa-flag"></i>
     <i class="fas fa-retweet"></i>
@@ -47,36 +48,30 @@ const createTweetElement = function(tweetGen) {
     </span>
     </div>
     </article>
+    `);
 
   return $tweet;
-}
+};
 
 const renderTweets = function(tweets) {
   // loops through tweets
   for (let tweet of tweets) {
     // calls createTweetElement for each tweet
-    const element = createTweetElement(tweet);
+    const generatedTweet = createTweetElement(tweet);
     // takes return value and appends it to the tweets container
-    $('.tweet-body'.prepend(element))
+    $('#tweets-container').prepend(generatedTweet);
   }
-}
-
-// // temporary hard coded tweets
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227
-// }
+};
 
 
-// const $tweet = createTweetElement(tweetData);
+//add an event listener that listens for the submit event
 
-// // Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// $('.tweets').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+//prevent the default behaviour of the submit event (data submission and page refresh)
+
+//create an AJAX POST request in client.js that sends the form data to the server.
+
+
+
+$(document).ready(() => {
+  renderTweets(data);
+});
