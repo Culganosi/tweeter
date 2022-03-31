ebbd193cf745105002646a54a7faf1395332451e
Culganosi <culgan_@hotmail.com>
2022-03-31 16:35:52 -0400

    responsive design && cleanup before submission && eslinted

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,87 +5,91 @@
  */
 // From server/data-files
 
-//PREVENT XSS WITH ESCAPING 
-const escape = function (str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
+//PREVENT XSS WITH ESCAPING
+const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
 };
 
 //FUNCTION TO GENERATE TWEETS
 const createTweetElement = function(tweetObject) {
-  //IMPORT HTML FROM INDEX AND IMPLEMENET INTO FUNCTION USING JQUERY
-  let $tweet = (`
+    //IMPORT HTML FROM INDEX AND IMPLEMENET INTO FUNCTION USING JQUERY
+    let $tweet = (`
   <article class="tweet">
     <header>
       <img src ="${escape(tweetObject.user.avatars)}" alt="avatar goes here" class="avatars">
-      <h2 class="handle">${escape(tweetObject.user.handle)}</h2>
-      <p class="name">${escape(tweetObject.user.name)}</p>
+      <h2 class="handle">${escape(tweetObject.user.name)}</h2>
+      <p class="name">${escape(tweetObject.user.handle)}</p>
     </header>
+    <body>
     <div class="tweets-main">
     <p class="content">${escape(tweetObject.content.text)}</p>
+    </body>
+    <footer>
     <span class="daysAgo">${timeago.format(tweetObject.created_at)}</span>
     <span class="icons">
     <i class="fas fa-flag"></i>
     <i class="fas fa-retweet"></i>
     <i class="fas fa-heart"></i>
     </span>
+    </footer>
     </div>
     </article>
     `);
 
-  return $tweet;
+    return $tweet;
 };
 
 //FUNCTION TO RENDER THE GENERATED TWEETS
 const renderTweets = function(tweets) {
-  // loops through tweets
-  for (let tweet of tweets) {
-    // calls createTweetElement for each tweet
-    const generatedTweet = createTweetElement(tweet);
-    // takes return value and appends it to the tweets container
-    $('#tweets-container').prepend(generatedTweet);
-  }
+    // loops through tweets
+    for (let tweet of tweets) {
+        // calls createTweetElement for each tweet
+        const generatedTweet = createTweetElement(tweet);
+        // takes return value and appends it to the tweets container
+        $('#tweets-container').prepend(generatedTweet);
+    }
 };
 // AJAX POST && ERRORS
 let addTweet = () => {
-  $('form').on('submit', function(event) {
-    event.preventDefault();
-    //ERROR TOO MANY CHARACTERS
-    if ($("#tweet-text").val().length > 140) {
-      $('.error-message').text("Error! You must limit your tweets to 140 characters!").slideDown();
-    // ERROR NO INPUT
-    } else if ($("#tweet-text").val().length === 0) {
-      $('.error-message').text("Please input text into the field").slideDown();
-    } else {
-    $('.error-message').slideUp();
-    $.ajax({
-      type: "POST",
-      url: "/tweets",
-      data: $(this).serialize(),
-    }).then(() => {
-      loadTweets();
-  }).catch();
-   //CLEAR INPUT FIELD AFTER POSTING A TWEET
-    $('.new-tweet').find('form').trigger('reset');
-   //RESET THE CHARACTER COUNTER AFTER POSTING A TWEET
-    $('.counter').text(140);
-  };
-})
+    $('form').on('submit', function(event) {
+        event.preventDefault();
+        //ERROR TOO MANY CHARACTERS
+        if ($("#tweet-text").val().length > 140) {
+            $('.error-message').text("Error! You must limit your tweets to 140 characters.").slideDown();
+            // ERROR NO INPUT
+        } else if ($("#tweet-text").val().length === 0) {
+            $('.error-message').text("Error! Please input text into the field").slideDown();
+        } else {
+            $('.error-message').slideUp();
+            $.ajax({
+                type: "POST",
+                url: "/tweets",
+                data: $(this).serialize(),
+            }).then(() => {
+                loadTweets();
+            }).catch();
+            //CLEAR INPUT FIELD AFTER POSTING A TWEET
+            $('.new-tweet').find('form').trigger('reset');
+            //RESET THE CHARACTER COUNTER AFTER POSTING A TWEET
+            $('.counter').text(140);
+        }
+    });
 };
-// Ajax GET
+// AJAX GET
 let loadTweets = () => {
-  $.ajax({
-    type: "GET",
-    url: "/tweets",
-    data: "data"
-  }).then((data) => {
-    renderTweets(data);
-  }).catch()
+    $.ajax({
+        type: "GET",
+        url: "/tweets",
+        data: "data"
+    }).then((data) => {
+        renderTweets(data);
+    }).catch();
 };
 
 
 $(document).ready(() => {
-addTweet();
-loadTweets();
+    addTweet();
+    loadTweets();
 });
--- public/scripts/composter-char-counter.js
+++ public/scripts/composter-char-counter.js
@@ -1,30 +1,13 @@
-// $(document).ready() {
-// const myTextArea = document.getElementById("#tweet-text");
-// const remainCharsText = document.getElementsByClassName('.counter');
-// const MAX_CHARS = 140;
-
-// myTextArea.addEventListener('input', () => {
-//   const remaining = MAX_CHARS - myTextArea.value.length;
-
-//   console.log(remaining)
-//   remainingCharsText.textContent = `${remaining} characters remaining`;
-// });
-// };
-
 $(document).ready(function () {
   let maxChars = 140;
 
   $("#tweet-text").keydown(function() {
-    
     let charsLeft = maxChars - $(this).val().length;
-
     $(".counter").text(charsLeft);
-
     if (charsLeft < 0) {
       $(".counter").addClass("red-text");
     } else {
       $(".counter").removeClass("red-text");
     }
-
   });
 });
